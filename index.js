3cf635e6a19511202801da5bdc6c8c52a19debec
Nithin Prem <nithinprem007@gmail.com>
2022-04-03 12:52:08 +0530

    Frontend Project complete

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const tourlistcard1 = document.querySelector(
 
 const infographic = document.querySelector(".infographics");
 const info = document.querySelector(".tourinfo");
+const tourcontent1 =
+	document.querySelector(".tourcontent1");
 
 tourlistcard1.addEventListener("click", (e) => {
 	infographic.classList.toggle("hidden");
 	info.classList.toggle("hidden");
+	tourcontent1.classList.toggle("hidden");
 });
 
-// **********************************************************************************************************
+// ****************************************************************
+
 const tourlistcard2 = document.querySelector(
 	`[data-tourlist="2"]`
 );
@@ -19,13 +23,16 @@ const infographic2 = document.querySelector(
 	".infographics2"
 );
 const info2 = document.querySelector(".tourinfo2");
+const tourcontent2 =
+	document.querySelector(".tourcontent2");
 
 tourlistcard2.addEventListener("click", (e) => {
 	infographic2.classList.toggle("hidden");
 	info2.classList.toggle("hidden");
+	tourcontent2.classList.toggle("hidden");
 });
 
-// **********************************************************************************************************
+// ****************************************************************
 
 const tourlistcard3 = document.querySelector(
 	`[data-tourlist="3"]`
@@ -35,28 +42,35 @@ const infographic3 = document.querySelector(
 	".infographics3"
 );
 const info3 = document.querySelector(".tourinfo3");
+const tourcontent3 =
+	document.querySelector(".tourcontent3");
 
 tourlistcard3.addEventListener("click", (e) => {
 	infographic3.classList.toggle("hidden");
 	info3.classList.toggle("hidden");
+	tourcontent3.classList.toggle("hidden");
 });
 
-// **********************************************************************************************************
+// ****************************************************************
 
 const tourlistcard4 = document.querySelector(
 	`[data-tourlist="4"]`
 );
+
 const infographic4 = document.querySelector(
 	".infographics4"
 );
 const info4 = document.querySelector(".tourinfo4");
+const tourcontent4 =
+	document.querySelector(".tourcontent4");
 
 tourlistcard4.addEventListener("click", (e) => {
 	infographic4.classList.toggle("hidden");
 	info4.classList.toggle("hidden");
+	tourcontent4.classList.toggle("hidden");
 });
 
-// **********************************************************************************************************
+// ****************************************************************
 
 const tourlistcard5 = document.querySelector(
 	`[data-tourlist="5"]`
@@ -66,13 +80,16 @@ const infographic5 = document.querySelector(
 	".infographics5"
 );
 const info5 = document.querySelector(".tourinfo5");
+const tourcontent5 =
+	document.querySelector(".tourcontent5");
 
 tourlistcard5.addEventListener("click", (e) => {
 	infographic5.classList.toggle("hidden");
 	info5.classList.toggle("hidden");
+	tourcontent5.classList.toggle("hidden");
 });
 
-// **********************************************************************************************************
+// ****************************************************************
 
 const tourlistcard6 = document.querySelector(
 	`[data-tourlist="6"]`
@@ -82,8 +99,11 @@ const infographic6 = document.querySelector(
 	".infographics6"
 );
 const info6 = document.querySelector(".tourinfo6");
+const tourcontent6 =
+	document.querySelector(".tourcontent6");
 
 tourlistcard6.addEventListener("click", (e) => {
 	infographic6.classList.toggle("hidden");
 	info6.classList.toggle("hidden");
+	tourcontent6.classList.toggle("hidden");
 });
